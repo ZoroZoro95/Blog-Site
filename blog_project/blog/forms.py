696005b4d1cfb7b5d6b0379187412d9158292b4d
ZoroZoro95 <connectwithsid95@gmail.com>
2023-10-01 17:54:09 +0530

    issue1 resolved

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('author','title', 'text',)
+        fields = ('Blog Author','Blog title', 'Blog Text',)
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
